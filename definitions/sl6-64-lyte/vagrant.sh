38f1e255763c8749328f20c63cb3d4b7e19af586
David Schoen <dave@lyte.id.au>
2014-03-17 11:33:24 +1100

    Deploy vagrant ssh keys for root too - normalise some config

--- definitions/sl6-64-lyte/vagrant.sh
+++ definitions/sl6-64-lyte/vagrant.sh
@@ -5,4 +5,10 @@ mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
 wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
-chown -R vagrant /home/vagrant/.ssh
+chown -R vagrant.vagrant /home/vagrant/.ssh
+
+# Installing vagrant ssh keys as root too
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+cp /home/vagrant/.ssh/authorized_keys /root/.ssh/authorized_keys
+chown -R root.root /root/.ssh
